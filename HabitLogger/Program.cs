fa182cdb6dc9ddb84e1022fafe55b75031f7c8aa
lvisr <lvis@mail.com>
2024-11-03 17:50:21 +0000

    first commit

---
+++ HabitLogger/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace HabitLogger
+{
+    class Program
+    {
+        static String[] menuOptions = new string[] {"c", "r", "u", "d", "0", "exit"};
+        static void Main(string[] args)
+        {
+            bool exitProgram = false;
+            string option;
+
+            while (!exitProgram)
+            {
+                DisplayMenu();
+                option = GetMenuInput();
+
+                switch (option)
+                {
+                    case "c":
+                        break;
+                    case "r":
+                        break;
+                    case "u":
+                        break;
+                    case "d":
+                        break;                        
+                    case "0":
+                    case "exit":
+                        exitProgram = true;
+                        break;
+                    default:
+                        exitProgram = true;
+                        break;
+                }
+            }
+        }
+
+        static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("*****    HABIT LOGGER    *****\n");
+            Console.WriteLine("C - Create entry");
+            Console.WriteLine("R - Read log");
+            Console.WriteLine("U - Update entry");
+            Console.WriteLine("D - Delete entry\n");
+            Console.WriteLine("0 - Exit");
+        }
+        static string GetMenuInput()
+        {
+            String? input;
+            do
+            {
+                Console.Write("\nType option C R U D or Exit: ");
+                input = Console.ReadLine();
+            } while (input is null || !menuOptions.Contains(input.ToLower()));
+            return input.ToLower();
+        }
+    }
+}
